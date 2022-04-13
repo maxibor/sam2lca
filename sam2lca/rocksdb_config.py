2ea35f0338a69cbcc06dcdc3fb463dfd6f1a65f9
maxibor <maxime.borry@gmail.com>
2022-04-13 09:45:02 +0200

    change rocksdb params

--- sam2lca/rocksdb_config.py
+++ sam2lca/rocksdb_config.py
@@ -2,18 +2,12 @@ import rocksdb
 
 OPTS_create = rocksdb.Options()
 OPTS_create.create_if_missing = True
-OPTS_create.max_open_files = 250
-OPTS_create.table_factory = rocksdb.BlockBasedTableFactory(
-    filter_policy=rocksdb.BloomFilterPolicy(10),
-    block_cache=rocksdb.LRUCache(6 * (1024**3)),
-    block_cache_compressed=rocksdb.LRUCache(1.5 * (1024**3)),
-)
+OPTS_create.max_open_files = 500
+OPTS_create.target_file_size_base = 10**8
+OPTS_create.target_file_size_multiplier = 3
 
 OPTS_read = rocksdb.Options()
 OPTS_read.create_if_missing = False
-OPTS_read.max_open_files = 250
-OPTS_read.table_factory = rocksdb.BlockBasedTableFactory(
-    filter_policy=rocksdb.BloomFilterPolicy(10),
-    block_cache=rocksdb.LRUCache(6 * (1024**3)),
-    block_cache_compressed=rocksdb.LRUCache(1.5 * (1024**3)),
-)
+OPTS_read.max_open_files = 500
+OPTS_read.target_file_size_base = 10**8
+OPTS_read.target_file_size_multiplier = 3
